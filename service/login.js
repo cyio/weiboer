e01a078e2ee0a06e849f12cfa5308c7dc0b8e9a6
cyio <icaner@qq.com>
2018-01-14 13:21:21 +0800

    改用手机版网页登陆

--- service/login.js
+++ service/login.js
@@ -3,20 +3,21 @@ const puppeteer = require('puppeteer');
 const delay = require('../utils/delay');
 function* login (page) {
   yield page.setUserAgent('Mozilla/5.0(Macintosh;U;IntelMacOSX10_6_8;en-us)AppleWebKit/534.50(KHTML,likeGecko)Version/5.1Safari/534.50');
-  yield page.goto('http://weibo.com');
-  yield page.waitForSelector('#loginname', {timeout: 20000});
-  yield page.click('#loginname');
-  yield page.focus('#loginname');
+  yield page.goto('https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=https%3A%2F%2Fm.weibo.cn%2Fbeta');
+  yield page.waitForSelector('#loginName', {timeout: 20000});
+  yield delay(1500);
+  yield page.click('#loginName');
+  yield page.focus('#loginName');
   const username = nconf.get('username');
   yield page.type(username);
   yield delay(1500);
-  yield page.click('input[type=password].W_input');
-  yield page.focus('input[type=password].W_input');
+  yield page.click('#loginPassword');
+  yield page.focus('#loginPassword');
   const password = nconf.get('password');
   yield page.type(password);
-  const submitBtn = yield page.$('a[node-type="submitBtn"]');
+  const submitBtn = yield page.$('#loginAction');
   yield submitBtn.click();
-  return page.waitForSelector('#skin_cover_s', 20000);
+  return page.waitForSelector('.icon_edit', 20000);
 }
 
 module.exports = login;
